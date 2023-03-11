fcfae083df31f88cc50046cbff1a95591c7201e0
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2023-03-11 11:26:24 +0100

    implemented the chat.router.js file and did some modifications

--- src/resources/chats/chat.router.js
+++ src/resources/chats/chat.router.js
@@ -1,1 +1,17 @@
+const express = require('express')
+const { Router } = express
+const controllers = require('./chat.controller')
 
+const router = Router()
+
+//for all routes
+router.route('/').get(controllers.fetchMany).post(controllers.createOne)
+
+//for a route with an id
+router
+  .route('/:id')
+  .get(controllers.fetchOne)
+  .put(controllers.updateOne)
+  .delete(controllers.deleteOne)
+
+module.exports = router
