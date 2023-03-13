9ae6d6f7ad46a9be7095bc678f5e4bc85b0faeeb
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2023-03-13 17:15:58 +0100

    implemented routes nested under user

--- src/resources/user/user.controller.js
+++ src/resources/user/user.controller.js
@@ -1,6 +1,7 @@
 const User = require('./user.model')
 const generateRandomAvatar = require('../data/generate.avatar')
 
+//creating a user account
 const userDetails = (req, res) => {
   res.status(200).json({ data: req.user })
 }
@@ -17,17 +18,91 @@ const updateUserDetails = async (req, res) => {
   }
 }
 
-// const createAvatarStyle = async (req, res) => {
-//   try {
-//     const avatarImage =
-//   } catch (e) {
-//     console.log(e.message)
-//     res.status(400).end()
-//   }
-// }
+const deleteOne = async (req, res) => {
+  try {
+    const deletedUser = await User.findOneAndRemove({ _id: req.params.id })
+    if (!deletedUser) {
+      res
+        .status(404)
+        .json({ message: `the user with ${req.params.is} is not found` })
+      res
+        .status(200)
+        .json({ data: deletedUser, message: 'user successful deleted' })
+    }
+  } catch (e) {
+    console.log(e.message)
+    res.status(404).end()
+  }
+}
+
+//fetching a user,users , users with an id ad their comments e.t.c
+const fetchOne = async (req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.params.id,
+    }).populate('posts')
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: `user with ${req.params.id} does not exist` })
+    }
+    res.status(200).json({ data: user })
+  } catch (e) {
+    console.log(e.message)
+    res.status(400).end()
+  }
+}
+
+const fetchMany = async (req, res) => {
+  try {
+    const users = await User.find({}).populate('posts')
+    if (!users) {
+      res.status(404).json({ message: 'no existing users for now' })
+    }
+    res.status(200).json({ data: users })
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+const fetchChatWithManyComments = async () => {
+  try {
+    const user = await User.findOne({})
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: `user with ${req.params.id} does not exist` })
+    }
+    res.status(200).json({ data: user })
+  } catch (e) {
+    console.log(e.message)
+    res.status(400).end()
+  }
+}
+
+const fetchChatWithOneComment = async () => {
+  try {
+    const user = await User.findOne({
+      _id: req.params.id,
+    })
+    if (!user) {
+      res
+        .status(404)
+        .json({ message: `user with ${req.params.id} does not exist` })
+    }
+    res.status(200).json({ data: user })
+  } catch (e) {
+    console.log(e.message)
+    res.status(400).end()
+  }
+}
 
 //exporting the controllers
 exports.userControllers = {
   userDetails: userDetails,
   updateUserDetails: updateUserDetails,
+  fetchOne: fetchOne,
+  fetchMany: fetchMany,
+  fetchChatWithManyComments: fetchChatWithManyComments,
+  fetchChatWithOneComment: fetchChatWithOneComment,
+  deleteOne: deleteOne,
 }
